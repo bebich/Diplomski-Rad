b1a8028badce46e97e42b501ec3260dcb3d8b97c
fbebek <finassistapp@gmail.com>
2021-06-24 17:25:01 +0200

    app.py

--- model/database_config.py
+++ model/database_config.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from application import app
+from app import app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
